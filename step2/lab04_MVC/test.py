e6a5b3e65e52df3128b366cc86f1c4f42ce5ba36
user <totoroliu0929@gmail.com>
2022-03-18 11:33:33 +0800

    modify

--- step2/lab04_MVC/test.py
+++ step2/lab04_MVC/test.py
@@ -1,2 +1,20 @@
-z="10"
-print(z*4)
+class Car:
+    def __init__(self, maker='', num_of_doors=4,  fuel_type='oil'):
+        self.maker, self.num_of_doors,  self.fuel_type = maker, num_of_doors,  fuel_type
+
+    def __repr__(self):
+        return 'I\'m a car.'
+
+class Bus:
+    def __init__(self, maker='', size=''):
+        self.maker, self.size = maker, size
+
+    def __repr__(self):
+        return 'I\'m a bus.'
+
+class Truck:
+    def __init__(self, maker='', weight=10):
+        self.maker, self.weight = maker, weight
+
+    def __repr__(self):
+        return 'I\'m a truck.'
